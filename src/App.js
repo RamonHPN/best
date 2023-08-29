e0ba5c618a0ec729e955530927570692c790949b
ramon henrique <ramonhenrique27r@gmail.com>
2023-08-29 10:43:21 -0300

    Perguntas, opções e pontuação funcionam.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import questions from './Perguntas';
+import React, { useState } from 'react';
+import verificarResposta from './Score';
 
 function App() {
+  const [perguntaAtual, setPerguntaAtual] = useState(0);
+  const [pontos, setPontos] = useState(0);
+  // const proximaPergunta = () => {
+  //   if (perguntaAtual < questions.length - 1) {
+  //     setPerguntaAtual(perguntaAtual + 1);
+  //   }
+  // }
+
+  const handleRespostaClick = (opcaoEscolhida) => {
+    const novaPontuacao = verificarResposta(perguntaAtual, opcaoEscolhida);
+    console.log(novaPontuacao);
+    setPontos(novaPontuacao);
+
+    if (perguntaAtual < questions.length - 1) {
+      setPerguntaAtual(perguntaAtual + 1);
+    }
+  };
+  
+
   return (
     // </div>
     <div>
@@ -30,23 +51,23 @@ function App() {
         <div class="parte1">
 
           <div class="divPergunta">
-            <h1 class="pergunta">Pergunta 1</h1>
+            <h1 class="pergunta">{questions[perguntaAtual].question}</h1>
 
           </div>
 
           <div class="divPontos">
             <h3 class="pontos">Pontos:</h3>
-            <h2>0</h2>
+            <h2>{pontos}</h2>
           </div>
 
         </div>
 
         <div class="parte2">
           <div class="opts">
-            <button class="btnOpt"><p>Opção 1</p></button>
-            <button class="btnOpt"><p>Opção 2</p></button>
-            <button class="btnOpt"><p>Opção 3</p></button>
-            <button class="btnOpt"><p>Opção 4</p></button>
+            <button class="btnOpt" onClick={() => handleRespostaClick(0)}><p>{questions[perguntaAtual].options[0]}</p></button>
+            <button class="btnOpt" onClick={() => handleRespostaClick(1)}><p>{questions[perguntaAtual].options[1]}</p></button>
+            <button class="btnOpt" onClick={() => handleRespostaClick(2)}><p>{questions[perguntaAtual].options[2]}</p></button>
+            <button class="btnOpt" onClick={() => handleRespostaClick(3)}><p>{questions[perguntaAtual].options[3]}</p></button>
           </div>
 
         </div>
--- src/Score.js
+++ src/Score.js
@@ -1,17 +1,16 @@
 import questions from './Perguntas.js';
 
 
+let score = 0; // Inicializa a pontuação fora da função
 
 function verificarResposta(perguntaIndex, opcaoEscolhida) {
-    var score = 0;
     if (opcaoEscolhida === questions[perguntaIndex].answer) {
-        score=score+1;
-    }
-    if (opcaoEscolhida !== questions[perguntaIndex].answer) {
-        score=score-1;
+        score = score + 1;
+    } else {
+        score = score - 1;
     }
     console.log(score);
     return score;
-};
+}
 
 export default verificarResposta;
